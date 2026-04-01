f8e68d569766f0170dd83520b0cc7bcc5275c000
Dmitriy Borovikov <dmitry@borovikov.name>
2026-04-01 16:39:50 +0300

    Update button driver: cleanup code, work full press

--- main/button_driver.cpp
+++ main/button_driver.cpp
@@ -12,7 +12,6 @@
 #include <app_priv.h>
 
 static const char *TAG = "button_driver";
-static bool perform_factory_reset = false;
 
 static const button_config_t button_config = {
     .long_press_time = CONFIG_BUTTON_LONG_PRESS_TIME_MS,
@@ -24,38 +23,25 @@ const button_gpio_config_t btn_gpio_cfg = {
     .active_level = 0,
 };
 
-static void app_driver_button_toggle_cb(void *arg, void *data)
+static void button_single_click_cb(void *arg, void *data)
 {
-    ESP_LOGI(TAG, "Toggle button pressed");
+    ESP_LOGI(TAG, "Toggle button press");
     void (*button_toggle_callback)() = (void (*)())data;
     button_toggle_callback();
 }
 
-
-static void button_factory_reset_pressed_cb(void *arg, void *data)
+static void button_long_press_up_cb(void *arg, void *data)
 {
-    if (!perform_factory_reset) {
-        ESP_LOGI(TAG, "Factory reset triggered. Release the button to start factory reset.");
-        perform_factory_reset = true;
-    }
-}
-
-static void button_factory_reset_released_cb(void *arg, void *data)
-{
-    if (perform_factory_reset) {
-        ESP_LOGI(TAG, "Starting factory reset");
-        void (*button_reset_callback)() = (void (*)())data;
-        button_reset_callback();
-        perform_factory_reset = false;
-    }
+    ESP_LOGI(TAG, "Button long press");
+    void (*button_reset_callback)() = (void (*)())data;
+    button_reset_callback();
 }
 
 void app_driver_button_init(void (*button_toggle_callback)(), void (*button_reset_callback)()) {
 	button_handle_t button_handle;
     esp_err_t err = iot_button_new_gpio_device(&button_config, &btn_gpio_cfg, &button_handle);
-    ABORT_APP_ON_FAILURE(button_handle != nullptr, ESP_LOGE(TAG, "Failed to create button handle"));
-	err |= iot_button_register_cb(button_handle, BUTTON_PRESS_DOWN, NULL, app_driver_button_toggle_cb, (void*)button_toggle_callback);
-    err |= iot_button_register_cb(button_handle, BUTTON_LONG_PRESS_HOLD, NULL, button_factory_reset_pressed_cb, (void*)button_reset_callback);
-    err |= iot_button_register_cb(button_handle, BUTTON_PRESS_UP, NULL, button_factory_reset_released_cb, NULL);
-    ESP_ERROR_CHECK(err);
+    ABORT_APP_ON_FAILURE(err == ESP_OK, ESP_LOGE(TAG, "Failed to create button handle"));
+	err |= iot_button_register_cb(button_handle, BUTTON_SINGLE_CLICK, NULL, button_single_click_cb, (void*)button_toggle_callback);
+    err |= iot_button_register_cb(button_handle, BUTTON_LONG_PRESS_UP, NULL, button_long_press_up_cb, (void*)button_reset_callback);
+    ABORT_APP_ON_FAILURE(err == ESP_OK, ESP_LOGE(TAG, "Failed to register callback"));
 }
